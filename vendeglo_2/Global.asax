702a18e31b5a1261d92dc64f10070b92ced7cadc
Ákos Kiss <akos.kiss26@gmail.com>
2019-11-18 20:54:55 +0100

    #1: a WPF alkalmazás varázsló futtatása

---
+++ vendeglo_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vendeglo_2.MvcApplication" Language="C#" %>
---
+++ vendeglo_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vendeglo_2.Startup))]
+namespace vendeglo_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
